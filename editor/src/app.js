6abac323ee6feca81a3ae2e0896df7ab6f6b1f17
dannygersh <dannygershman96@gmail.com>
2024-03-16 17:19:31 +0200

    basic latex functionallity

--- editor/src/app.js
+++ editor/src/app.js
@@ -1,6 +1,21 @@
 const axios = require('axios');
 import './index.css';
 
+const POST_URL = 'http://localhost:3002/';
+
+let latx_session = false;
+function latexUnfocuse() {
+  if(latx_session?.setAttribute) {
+    latx_session?.setAttribute('class', 'latex');
+  }
+  latx_session = false;
+}
+function latexFocuse(node) {
+  node?.setAttribute('class', 'latex-selected');
+  latx_session = node;
+}
+
+// init
 const quill = new Quill('#editor', {
   theme: 'snow',
   modules: {
@@ -17,73 +32,115 @@ const quill = new Quill('#editor', {
     },
   },
 });
-
 const toolbarLatexBtn = document.querySelector('.ql-toolbarLatexBtn');
 if (toolbarLatexBtn) {
   toolbarLatexBtn.innerHTML = 'Σ';
 }
-
-let latx_session = false;
-function toolbarLatexHandler() {
-  const index = quill.getSelection().index || 0;
-  quill.insertEmbed(index, 'customImage', { url: "https://clipart-library.com/image_gallery/396690.png" });
-  quill.setSelection(index + 1);
-
-  latx_session = quill.getLeaf(index)[0];
+// editor focuse event
+document.getElementById("editor").firstChild.onfocus = () => {
+  latexUnfocuse();
 }
-
-
+// latex blot
 class ImageBlot extends Quill.import('blots/embed') {
   static create(value) {
     const node = super.create();
     node.setAttribute('src', value.url);
     node.setAttribute('alt', value.alt || '');
     node.setAttribute('class', 'latex');
+
     node.addEventListener('click', () => {
-      console.log("TODO")
+      if(latx_session != node && latx_session) {
+        // replace exicting latex_session with new one
+        latexUnfocuse();
+        latexFocuse(node);
+        quill.blur();
+      } else if(latx_session != node) {
+        // create new latx_session
+        latexFocuse(node);
+        quill.blur();
+      } else {
+        // exicting latx_session is clicked
+        latexUnfocuse();
+      }
+      
     });
     return node;
   }
   static value(node) {
     return { url: node.getAttribute('src'), alt: node.getAttribute('alt') };
   }
+
+  detach() { // omn blot removal
+    super.detach();
+    latexUnfocuse();
+  }
 }
 ImageBlot.blotName = 'customImage';
 ImageBlot.tagName = 'img';
 Quill.register(ImageBlot);
 
+// create new latex
+function toolbarLatexHandler() {
+  
+  latexUnfocuse();
 
-const post_url = 'http://localhost:3002/';
-document.getElementById('latex-btn').addEventListener('click', () => {
+  const index = quill.getSelection().index || 0;
+  quill.insertEmbed(index, 'customImage', { url: "https://clipart-library.com/image_gallery/396690.png" });
+  quill.setSelection(index + 1);
 
-  const value = document.getElementById('latex-textarea')?.value;
+  latx_session = quill.getLeaf(index + 1)[0];
+}
 
-  axios.post(post_url, value, {
-    headers: {
-      'Content-Type': 'text/plain',
-    },
-  })
-    .then(res => {
-      const resultElm = document.getElementById('latex-result');
-      if(!res?.data?.success) resultElm.value = res.data.msg;
-      else {
-        resultElm.value = "success";
-        console.log(res?.data?.resSvgFilePath);
+// compile button
+document.getElementById('compile-btn').addEventListener('click', () => {
 
-        if (latx_session && latx_session.domNode.tagName === "IMG") {
-          latx_session.domNode.src = `http://127.0.0.1:3002/${res?.data?.resSvgFilePath}`;
+  // if the compile-btn was pressed,
+  // this must mean that latx_session is not false
+
+  if (latx_session) {
+
+    const value = document.getElementById('latex-textarea')?.value;
+
+    // send latex to the compiler
+
+    axios.post(POST_URL, value, {
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+    })
+      .then(res => {
+        const resultElm = document.getElementById('latex-result');
+        if (!res?.data?.success) resultElm.value = res.data.msg;
+        else {
+
+          resultElm.value = "success";
+
+          if(latx_session?.domNode?.src) {
+            latx_session.domNode.src = `http://127.0.0.1:3002/${res?.data?.resSvgFilePath}`;
+          } else if (latx_session?.src) {
+            latx_session.src = `http://127.0.0.1:3002/${res?.data?.resSvgFilePath}`;
+          } else {
+            console.log("ERROR 23462346"); // TODO
+          }
+
+          latexUnfocuse();
+
         }
-        latx_session = false;
-      }
-    })
-    .catch(error => {
-      alert("something whent wrong");
-      if (error.response) {
-        // The request was made, but the server responded with a status code outside the range of 2xx
-        console.error('Error:', error.response.data);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.error('Error:', error.message);
-      }
-    })
+      })
+      .catch(error => {
+        alert("something whent wrong");
+        if (error.response) {
+          // TODO
+          // The request was made, but the server responded with a status code outside the range of 2xx
+          console.error('Error:', error.response.data);
+        } else {
+          // TODO
+          // Something happened in setting up the request that triggered an Error
+          console.error('Error:', error.message);
+        }
+      })
+
+  } else {
+    // TODO: this should never happen
+  }
 });
