b5751edaf3fecb5d43717427edb70dbd36fe31b8
dannygersh <dannygershman96@gmail.com>
2024-03-16 17:18:30 +0200

    updated compile.bash

--- compiler/compile.bash
+++ compiler/compile.bash
@@ -6,13 +6,15 @@ mkdir -p tmp
 touch tmp/texput.tex
 echo "$1" > tmp/texput.tex
 
-timeout 1s pdflatex -interaction=batchmode -halt-on-error --output-directory="$(pwd)/tmp" tmp/texput.tex
+#timeout 1s latex -interaction=batchmode -halt-on-error --output-directory="$(pwd)/tmp" tmp/texput.tex
+latex -interaction=batchmode -halt-on-error -output-directory="$(pwd)/tmp" tmp/texput.tex
 if [ $? -ne 0 ]; then
   exit -6;
 fi
 
-pdfcrop tmp/texput.pdf tmp/texput.pdf
-pdf2svg tmp/texput.pdf tmp/$2.svg
+#pdfcrop tmp/texput.pdf tmp/texput.pdf
+#pdf2svg tmp/texput.pdf tmp/$2.svg
+dvisvgm --no-fonts -o tmp/$2.svg tmp/texput.dvi
 if [ $? -ne 0 ]; then
   exit -7;
 fi
