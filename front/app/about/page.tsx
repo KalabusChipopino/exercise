f442588e6e57e30bd4eb902bf13c4207e6e4d466
vadim gershman <vadim1454@gmail.com>
2024-05-20 21:47:29 +0300

    test no.2

--- front/app/about/page.tsx
+++ front/app/about/page.tsx
@@ -1,6 +1,6 @@
 export default function Profile() {
-    return <div className=" w-full px-[15vw] pt-[10px]">
-        this site was built because the exercises in the university suck, 
+    return <div className=" w-full px-[15vw] pt-[20px]">
+        This site was built because the exercises in the university suck, 
         they are not helpfull and only test your knowladge instead of 
         teaching. you can create, edit, like share, comment and export 
         exercises that you or others create.
