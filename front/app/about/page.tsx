9e5831058bf2247eba3b37bc68affd944959449f
vadim gershman <vadim1454@gmail.com>
2024-05-20 20:35:24 +0300

    test

--- front/app/about/page.tsx
+++ front/app/about/page.tsx
@@ -1,3 +1,8 @@
 export default function Profile() {
-    return <div>About</div>
+    return <div className=" w-full px-[15vw] pt-[10px]">
+        this site was built because the exercises in the university suck, 
+        they are not helpfull and only test your knowladge instead of 
+        teaching. you can create, edit, like share, comment and export 
+        exercises that you or others create.
+    </div>
 }
