06f59041df9b8f560238d577951917c01545d58c
vadim gershman <vadim1454@gmail.com>
2024-05-20 23:11:20 +0300

    main title

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -2,7 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className=" px-[5vw] pt-[10px]">
+      <h1 className="text-[22px]">
+        Title
+      </h1>
       Home
     </main>
   );
