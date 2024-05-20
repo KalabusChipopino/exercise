ff940dbc99cc9d9cf8616deae7f5d23a4e80e4b3
vadim gershman <vadim1454@gmail.com>
2024-05-21 00:41:00 +0300

    cards main

---
+++ front/app/components/CardMain.tsx
@@ -0,0 +1,9 @@
+export default function CardMain() {
+    return <div className="w-[43vw] h-[20vw] overflow-hidden border-[1px] 
+      px-[5px]  border-border">
+       it follows that the limit a
+        is equal with 2
+       . Above I used the Lagranges mean value theorem and the monotonicity of f′
+        on the interval 2,∞
+    </div>
+}
--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,12 +1,19 @@
 import Image from "next/image";
+import CardMain from "./components/CardMain";
 
 export default function Home() {
   return (
     <main className=" px-[5vw] pt-[10px]">
-      <h1 className="text-[22px]">
-        Title
-      </h1>
-      Home
+      <div className="flex w-full justify-between">
+        <h1 className="text-[22px]">
+          Title
+        </h1>
+        buttons
+      </div>
+      <div className="flex justify-between mt-[20px]">
+        <CardMain/>
+        <CardMain/>
+      </div>
     </main>
   );
 }
